1134b684c5f764f2fe491fe8d9bfcab84685ccd8
Stefano Gatta <stefano_gatta@iname.com>
2025-09-22 21:11:41 +0200

    Added name to home

--- website/views.py
+++ website/views.py
@@ -1,5 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
 
-# Create your views here.
 def home(request):
 	return render(request, 'home.html', {})
+
+def login_view(request):
+	if request.method == 'POST':
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.success(request, f'Welcome {username}!')
+				return redirect('home')
+			else:
+				messages.error(request, 'Invalid username or password.')
+	else:
+		form = AuthenticationForm()
+	return render(request, 'login.html', {'form': form})
+
+def register_view(request):
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			username = form.cleaned_data.get('username')
+			messages.success(request, f'Account created for {username}!')
+			login(request, user)
+			return redirect('home')
+	else:
+		form = UserCreationForm()
+	return render(request, 'register.html', {'form': form})
+
+def logout_view(request):
+	logout(request)
+	messages.success(request, 'You have been logged out.')
+	return redirect('home')
